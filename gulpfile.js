2b6bfb053325a3090725d31d5ce01289c457e72d
silasb <silas.baronda@gmail.com>
2014-08-20 20:00:11 -0400

    Nice way to handle errors with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var browserify = require('gulp-browserify');
 
 gulp.task('jsx', function() {
   return gulp.src('src/app.jsx', {read: false})
+    .pipe(plumber())
     .pipe(browserify({
       transform: ['reactify'],
       extensions: ['.jsx']
@@ -14,6 +16,7 @@ gulp.task('jsx', function() {
 
 gulp.task('css', function() {
   return gulp.src('src/style.css')
+    .pipe(plumber())
     .pipe(rename('style.css'))
     .pipe(gulp.dest('public/'))
 })
